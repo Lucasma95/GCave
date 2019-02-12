bcc8ab55c1075335327531ccc9c588b087d4baab
LucasAlvarez <lucasalvsoft@gmail.com>
2019-02-12 04:35:28 -0300

    minimal changues

--- GCave/src/main/java/com/bootcamp/GCave/controller/ModController.java
+++ GCave/src/main/java/com/bootcamp/GCave/controller/ModController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping(path="/users")
+@RequestMapping(path="/mods")
 public class ModController {
 
     @Autowired
--- GCave/src/main/java/com/bootcamp/GCave/controller/UserController.java
+++ GCave/src/main/java/com/bootcamp/GCave/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@RestController
+@RequestMapping(path="/users")
 public class UserController {
 
     @Autowired
